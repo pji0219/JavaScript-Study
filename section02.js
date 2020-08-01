c1a082e66bbd4c50a31ba89e351dd4d6ceef9e95
park jong ik <pji219@naver.com>
2020-08-01 19:43:29 +0900

    변수와 상수

---
+++ section02.js
@@ -0,0 +1,40 @@
+// 변수와 상수 
+
+// 변수
+let value = 1; // 변수 선언을 할때는 let 키워드를 사용한다.
+console.log(value);
+value = 2; // 변수는 한번 선언한 값을 바꿀 수 있다.
+console.log(value);
+
+// 상수
+
+const a = 1; // 상수를 선언 할때는 const 키워드를 사용한다.
+             // 상수는 한번 선언한 값을 바꿀 수 없다.
+
+console.log(a);
+
+
+// 기본적인 데이터 타입
+
+let number = 1;
+
+let string = '문자';
+
+console.log(number);
+console.log(string);
+
+let boolean1 = true;
+let boolean2 =  false;
+
+console.log(boolean1);
+console.log(boolean2);
+
+const none1 = null; // 값이 없다.
+const none2 = undefined // 아직 값이 설정되지 않았다.
+
+console.log(none1);
+console.log(none2);
+
+
+
+
