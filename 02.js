be8de1dffa3b2df6e086ae28ad4bea20ccbe9c92
park jong ik <pji219@naver.com>
2021-03-12 23:53:41 +0900

    1

---
+++ 02.js
@@ -0,0 +1,19 @@
+function increaseAndPrint(n) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const value = n + 1;
+            if(value === 5) {
+                const error = new Error();
+                error.name = 'ValueIsFiveError';
+                reject(error);
+                return;
+            }
+            console.log(value);
+            resolve(value);
+        })
+    }, 1000);
+}
+
+increaseAndPrint(0).then(n => {
+    console.log('result: ', n);
+})
